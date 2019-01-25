adb24b53869be6a17bae0b88130bccf9f0d1601c
mkotowski <mkotowski101@gmail.com>
2019-01-25 22:10:13 +0100

    Player HUD WIP

---
+++ GGJ6/Assets/Scenes/TestHud/PlayerHud.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHud : MonoBehaviour
+{
+    public House House { get; set; }
+
+    private Text playerName;
+    private Text playerScore;
+
+    private Toggle hasMale;
+    private Toggle hasFemale;
+    private Toggle hasChild;
+    private Toggle hasPet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
