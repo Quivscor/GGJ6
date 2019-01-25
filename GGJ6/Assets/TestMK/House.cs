59a45a1f1c4b42f995a41224bb30cce79087a0af
mkotowski <mkotowski101@gmail.com>
2019-01-25 21:19:14 +0100

    Add House.cs script

---
+++ GGJ6/Assets/TestMK/House.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    //NOTE: remove after implementation !!!
+    public class NPC : MonoBehaviour  { public int GetScore( ) { return 1; } public string GetType() { return "d"; } }
+    public class Player { }
+    //NOTE: REMOVE ^^^^^^^^^^^^^^^^^^^^^^^^^
+
+    private int score = 0;
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    void UpdateScore(int amount)
+    {
+        if ((score - amount) >= 0)
+        {
+            score += amount;
+        }
+
+    }
+
+    private bool hasFemale;
+    private bool hasChild;
+    private bool hasMale;
+    private bool hasPet;
+
+    // is there a complete set of family members?
+    // types: pet, female, male, child
+    private bool isFullSet;
+
+    bool IsFullSet()
+    {
+        return isFullSet;
+    }
+
+    void UpdateHousehold(string typeString)
+    {
+        switch (typeString)
+        {
+            case "female":
+                hasFemale = true;
+                break;
+            case "child":
+                hasChild = true;
+                break;
+            case "male":
+                hasMale = true;
+                break;
+            case "pet":
+                hasPet = true;
+                break;
+            default:
+                break;
+        }
+
+        if (hasFemale && hasMale && hasChild && hasPet)
+        {
+            isFullSet = true;
+        }
+    }
+
+    public Player Owner { get; set; }
+
+    void UpdateStats(NPC npc)
+    {
+        UpdateHousehold(npc.GetType());
+        UpdateScore(npc.GetScore());
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "NPC")
+        {
+            UpdateStats(collision.gameObject.GetComponent<NPC>());
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFullSet = false;
+
+        hasFemale = false;
+        hasChild  = false;
+        hasMale   = false;
+        hasPet    = false;
+
+        Owner = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
