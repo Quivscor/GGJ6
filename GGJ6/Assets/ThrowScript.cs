7e3b7d64b22078f4bc895962f3a624c7b10b4e9a
Bociek <bociek428@gmail.com>
2019-01-26 20:59:52 +0100

    Changed nothing Fixed everything Kill me

--- GGJ6/Assets/ThrowScript.cs
+++ GGJ6/Assets/ThrowScript.cs
@@ -16,7 +16,7 @@ public class ThrowScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        movementController = GetComponent<MovementController>();
+    movementController = GetComponent<MovementController>();
         Debug.Log("Action" + playerNumber);
     }
 
