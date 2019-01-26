abe38726e39f94f445e3d79f76b9e3725b4c182e
Bociek <bociek428@gmail.com>
2019-01-26 11:09:04 +0100

    Add multiple player support

--- GGJ6/Assets/MovementController.cs
+++ GGJ6/Assets/MovementController.cs
@@ -16,6 +16,7 @@ public class MovementController : MonoBehaviour
     {
         throwScript = GetComponent<ThrowScript>();
         body = GetComponent<Rigidbody2D>();
+        Debug.Log("Horizontal" + throwScript.playerNumber);
     }
 
     // Update is called once per frame
@@ -23,11 +24,10 @@ public class MovementController : MonoBehaviour
     {
         if (!throwScript._isStunned)
         {
-            body.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * speed, 1f), Mathf.Lerp(0, Input.GetAxis("Vertical") * speed, 1f));
-            //body.AddForce(new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * speed, 1f),Mathf.Lerp(0, Input.GetAxis("Vertical") * speed, 1f)));
-            if (Input.GetAxis("Horizontal") != 0.0f || Input.GetAxis("Vertical") != 0.0f)
+            body.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal" + throwScript.playerNumber) * speed, 1f), Mathf.Lerp(0, Input.GetAxis("Vertical" + throwScript.playerNumber) * speed, 1f));
+            if (Input.GetAxis("Horizontal"+ throwScript.playerNumber) != 0.0f || Input.GetAxis("Vertical" + throwScript.playerNumber) != 0.0f)
             {
-                directionVector2 = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+                directionVector2 = new Vector2(Input.GetAxis("Horizontal" + throwScript.playerNumber), Input.GetAxis("Vertical" + throwScript.playerNumber)).normalized;
             }
         }
         
