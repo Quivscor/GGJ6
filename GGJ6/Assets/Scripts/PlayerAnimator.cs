2e850bbe4e4d4c4d699b9bfa8fd6a360e4549cd7
Quivscor <quivscor@gmail.com>
2019-01-26 12:13:23 +0100

    Add Animator basics

---
+++ GGJ6/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator animator;
+    MovementController controller;
+    ThrowScript throwing;
+
+    int direction = -1; // 0 is up; 1 is left; 2 is right; 3 is down;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = gameObject.GetComponentInParent<MovementController>();
+        throwing = gameObject.GetComponentInParent<ThrowScript>();
+    }
+
+    private void LateUpdate()
+    {
+        if (controller.directionVector2.y > 0.5f && controller.directionVector2.x > -0.5f && controller.directionVector2.x < 0.5f)
+            direction = 0;
+        else if (controller.directionVector2.y < 0.5f && controller.directionVector2.y > -0.5f && controller.directionVector2.x < -0.5f)
+            direction = 1;
+        else if (controller.directionVector2.y < 0.5f && controller.directionVector2.y > -0.5f && controller.directionVector2.x > 0.5f)
+            direction = 2;
+        else if (controller.directionVector2.y < -0.5f && controller.directionVector2.x > -0.5f && controller.directionVector2.x < 0.5f)
+            direction = 3;
+
+        Debug.Log("Direction = " + direction);
+
+        if (throwing.holdsSth)
+        {
+            if (controller.isMoving)
+            {
+                //walk with holding
+            }
+            else
+            {
+                //stand with holding
+            }
+        }
+        else
+        {
+            if (controller.isMoving)
+            {
+                //walk without holding
+            }
+            else
+            {
+                //stand without holding
+            }
+        }
+    }
+}
