f22fcfc2d993201b6d72433c54d7f6f824c47670
Quivscor <quivscor@gmail.com>
2019-01-26 11:43:46 +0100

    Add random spawn

--- GGJ6/Assets/Spawner.cs
+++ GGJ6/Assets/Spawner.cs
@@ -17,6 +17,7 @@ public class Spawner : MonoBehaviour
         Random random = new Random();
         int index = Random.Range(0, spawnerList.Count - useless);
         Debug.Log(index);
-        return Instantiate(spawnerList[index]);
+        return Instantiate(spawnerList[index], new Vector3(this.transform.position.x + Random.Range(-2.0f,2.0f),
+            this.transform.position.y + Random.Range(-2.0f,2.0f),this.transform.position.z), Quaternion.identity);
     }
 }
