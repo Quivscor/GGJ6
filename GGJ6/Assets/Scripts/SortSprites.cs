972a00b1e03be1f46c7dc156abd7c46a7d291e52
Quivscor <quivscor@gmail.com>
2019-01-26 14:50:08 +0100

    Add more map features

---
+++ GGJ6/Assets/Scripts/SortSprites.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortSprites : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        GetComponent<SpriteRenderer>().sortingOrder = -(int)(transform.position.y * 100);
+    }
+}
